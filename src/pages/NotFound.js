4243f08f7168b6984faf2d6eeb4ec5fe3ffff217
Lateefat Obisesan <temilele06@gmail.com>
2026-05-27 19:54:10 -0500

    adding contents to not-found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
-  return <></>;
+  return 
+    <div className="not"> 
+        <div className="page">
+            <h2>404</h2>
+            
+        </div>
+    </div>;
 }
 
 export default NotFound;
