2120a1c91e24ed376b43f2ccd65d738bff2d8d4f
Lateefat Obisesan <temilele06@gmail.com>
2026-05-27 19:57:14 -0500

    added my contect to not-found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,7 +5,9 @@ function NotFound() {
     <div className="not"> 
         <div className="page">
             <h2>404</h2>
-            
+            <p>This page has wandered off.</p>
+            <p>The page you are looking for does not exist or has been moved.</p>
+            <Link to="/">Back to Essentials Hub</Link>
         </div>
     </div>;
 }
