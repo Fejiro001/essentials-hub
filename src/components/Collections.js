810cd889ccb350a3a6c48d8d8de9fdb602f43eb4
Fejiro001 <aberefejiro@gmail.com>
2026-05-28 20:14:10 -0500

    Removed code duplication

--- src/components/Collections.js
+++ src/components/Collections.js
@@ -3,6 +3,33 @@ import womenCollect from "../media/women.png";
 import jewelryCollect from "../media/jewelry.png";
 import electronicsCollect from "../media/electronics.png";
 
+const COLLECTIONS = [
+  {
+    key: "men's",
+    title: "Men's",
+    image: menCollect,
+    alt: "Mens Section"
+  },
+  {
+    key: "women's",
+    title: "Women's",
+    image: womenCollect,
+    alt: "Womens Section"
+  },
+  {
+    key: "jewelery",
+    title: "Jewelry",
+    image: jewelryCollect,
+    alt: "Jewelry Section"
+  },
+  {
+    key: "electronics",
+    title: "Electronics",
+    image: electronicsCollect,
+    alt: "Electronics Section"
+  }
+];
+
 function Collections() {
   return (
     <section className="collection">
@@ -10,52 +37,22 @@ function Collections() {
         <p>CHAPTER 01 - COLLECTIONS</p>
         <h2 className="header-collect">OUR COLLECTIONS</h2>
       </div>
-      <div className="collection-imgs">
-        <div className="box-img">
-          <img
-            src={menCollect}
-            className="collection-sing"
-            alt="Mens Section"></img>
-          <div className="box-img-text">
-            <h3>Men's</h3>
-            <p className="gray">SHOP COLLECTION</p>
-          </div>
-          <div className="overlay"></div>
-        </div>
-        <div className="box-img">
-          <img
-            src={womenCollect}
-            className="collection-sing"
-            alt="Womens Section"></img>
-          <div className="box-img-text">
-            <h3>Women's</h3>
-            <p className="gray">SHOP COLLECTION</p>
-          </div>
-          <div className="overlay"></div>
-        </div>
-        <div className="box-img">
-          <img
-            src={jewelryCollect}
-            className="collection-sing"
-            alt="Jewelry Section"></img>
-          <div className="box-img-text">
-            <h3>Jewelry</h3>
-            <p className="gray">SHOP COLLECTION</p>
-          </div>
-          <div className="overlay"></div>
-        </div>
-        <div className="box-img">
-          <img
-            src={electronicsCollect}
-            className="collection-sing"
-            alt="Electronics Section"></img>
-          <div className="box-img-text">
-            <h3>Electronics</h3>
-            <p className="gray">SHOP COLLECTION</p>
-          </div>
-          <div className="overlay"></div>
-        </div>
-      </div>
+      <ul className="collection-imgs">
+        {COLLECTIONS.map((collection) => (
+          <li className="box-img" key={collection.title}>
+            <img
+              src={collection.image}
+              className="collection-sing"
+              alt={collection.alt}
+            />
+            <div className="box-img-text">
+              <h3>{collection.title}</h3>
+              <p className="gray">SHOP COLLECTION</p>
+            </div>
+            <div className="overlay"></div>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
