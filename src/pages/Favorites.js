8339d2182394342a29acd34f70a7603975a63cfb
Lateefat Obisesan <temilele06@gmail.com>
2026-05-28 20:38:48 -0500

    all done

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { LuHeart, LuArrowLeft } from "react-icons/lu";
 import "../css/Favorites.css";
 
