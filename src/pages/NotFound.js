39769a797dfc6cf0438df3cba39fee8a49b8bbd4
Fejiro001 <aberefejiro@gmail.com>
2026-05-29 07:32:31 -0500

    Fixed 404 page styling

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,12 +3,12 @@ import "../css/NotFound.css";
 
 function NotFound() {
   return (
-    <main className="not">
+    <main className="not container">
       <section className="page">
         <h2>404</h2>
         <p>This page has wandered off.</p>
-        <p>The page you are looking for does not exist or has been moved.</p>
-        <Link to="/">Back to Essentials Hub</Link>
+        <p>The page you are looking for does not exist or has been moved. Let us get you back on track.</p>
+        <Link className="primary-btn" to="/">Back to Essentials Hub</Link>
       </section>
     </main>
   );
