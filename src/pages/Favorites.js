6d18ef4a9c3957fbad8845202052aa4d129a2b80
Fejiro001 <aberefejiro@gmail.com>
2026-05-29 07:44:52 -0500

    Styling improved

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -5,45 +5,56 @@ import "../css/Favorites.css";
 function Favorites() {
   const favoritesList = [];
 
-  if (favoritesList.length === 0) {
-    return (
-      <main className="favorites-page-empty container">
-        <div className="empty">
-          <LuHeart className="empty-icon" />
-          <h2>Your Wishlist is Empty</h2>
-          <p>Items you save while browsing will appear right here!</p>
-          <Link to="/catalog" className="explore-btn">
-            Explore Catalog
-          </Link>
-        </div>
-      </main>
-    );
-  }
   return (
-    <main className="favorites-page container">
-      <section className="favorites-header">
-        <Link className="back-to-catalog" to="/catalog">
-          <LuArrowLeft /> Back To Catalog
-        </Link>
-        <h2>My Favorite Items</h2>
-        <p>You have {favoritesList.length} saved essentials</p>
+    <main className="favorites-page">
+      <section className="secondary-banner">
+        <div className="container">
+          <h2>Favorites</h2>
+          <p>Items you've saved</p>
+        </div>
       </section>
-      <section className="favorites-grid">
-        {favoritesList.map((item) => (
-          <div key={item.id} className="favorite-card">
-            <div className="img-container">
-              <img src={item.image} alt={item.title} />
-            </div>
-            <div className="card-details">
-              <h3>{item.title}</h3>
-              <span className="price">${item.price}</span>
-              <Link to={`/catalog/${item.id}`} className="view-product-btn">
-                View Item
+      <div className="container">
+        {favoritesList.length === 0 ? (
+          <div className="favorites-page-empty">
+            <section className="empty">
+              <LuHeart className="empty-icon" />
+              <h2>Your Wishlist is Empty</h2>
+              <p>Items you save while browsing will appear right here!</p>
+              <Link to="/catalog" className="explore-btn">
+                Explore Catalog
               </Link>
-            </div>
+            </section>
           </div>
-        ))}
-      </section>
+        ) : (
+          <>
+            <section className="favorites-header">
+              <Link className="back-to-catalog" to="/catalog">
+                <LuArrowLeft /> Back To Catalog
+              </Link>
+              <h2>My Favorite Items</h2>
+              <p>You have {favoritesList.length} saved essentials</p>
+            </section>
+            <section className="favorites-grid">
+              {favoritesList.map((item) => (
+                <div key={item.id} className="favorite-card">
+                  <div className="img-container">
+                    <img src={item.image} alt={item.title} />
+                  </div>
+                  <div className="card-details">
+                    <h3>{item.title}</h3>
+                    <span className="price">${item.price}</span>
+                    <Link
+                      to={`/catalog/${item.id}`}
+                      className="view-product-btn">
+                      View Item
+                    </Link>
+                  </div>
+                </div>
+              ))}
+            </section>
+          </>
+        )}
+      </div>
     </main>
   );
 }
