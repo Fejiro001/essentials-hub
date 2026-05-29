5296d771fe9499ba2a78cd8312d8c962b0775712
Fejiro001 <aberefejiro@gmail.com>
2026-05-28 21:35:59 -0500

    Made marquee animated

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -1,9 +1,29 @@
-function Marquee () {
-    return (
-        <div className='line'>
-            <p>NEW ARRIVALS / ELECTRONICS / MEN'S CLOTHING / WOMEN'S CLOTHING / JEWELRY / ESSENTIALS / NEW ARRIVALS / ELECTRONICS / MEN'S CLOTHING</p>
-        </div>
-    )
+const MARQUEE_ITEMS = [
+  "NEW ARRIVALS",
+  "ELECTRONICS",
+  "MEN'S CLOTHING",
+  "WOMEN'S CLOTHING",
+  "JEWELRY",
+  "ESSENTIALS"
+];
+
+function Marquee() {
+  return (
+    <div className="line">
+      <div className="marquee-track">
+        {[...MARQUEE_ITEMS, ...MARQUEE_ITEMS, ...MARQUEE_ITEMS].map(
+          (item, i) => (
+            <>
+              <span key={i} className="marquee-item">
+                {item}
+              </span>
+              <span>/</span>
+            </>
+          )
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Marquee;
