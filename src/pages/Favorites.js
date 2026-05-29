624a3cc8e4bc6eff7f7814f6ff0c0e2d3ba42369
Lateefat Obisesan <temilele06@gmail.com>
2026-05-28 19:43:09 -0500

    added links to view the favorite items

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -7,7 +7,7 @@ function Favorites () {
 
     if (favoritesList.Length === 0) {
         return (
-            <main className="favorites-page conatiner">
+            <main className="favorites-page-empty conatiner">
                 <div className="empty">
                     <LuHeart className="empty-icon"/>
                     <h2>Your Wishlist is Empty</h2>
@@ -19,7 +19,33 @@ function Favorites () {
             </main>
         );
     }
-    return <></>
+    return (
+        <main className="favorites-page container">
+        <section className="favorites-header">
+        <Link className="back-to-catalog" to="/catalog">
+          <LuArrowLeft /> Back To Catalog
+        </Link>
+        <h2>My Favorite Items</h2>
+        <p>You have {favoritesList.length} saved essentials</p>
+      </section>
+      <section className="favorites-grid">
+        {favoritesList.map((item) => (
+          <div key={item.id} className="favorite-card">
+            <div className="img-container">
+              <img src={item.image} alt={item.title} />
+            </div>
+            <div className="card-details">
+              <h3>{item.title}</h3>
+              <span className="price">${item.price}</span>
+              <Link to={`/catalog/${item.id}`} className="view-product-btn">
+                View Item
+              </Link>
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
+    );
 }
 
 export default Favorites;
